927a7388f226027a746e388810885238b3034804
silent-hyena <ayushpanwar00007@gmail.com>
2026-03-12 22:27:29 +0530

    prepare backend for vercel deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,22 +10,23 @@ import EmailRoute from "./router/sendEmail.js";
 import staffRoute from "./router/staff.js";
 import studentRoute from "./router/student.js";
 import admissionRoute from "./router/admission.js";
-import adminRoute from "./router/admin.js"
-import instructor from "./router/instructor.js"
+import adminRoute from "./router/admin.js";
+import instructor from "./router/instructor.js";
 import { logout } from "./controller/logoutUser.js";
-
 
 dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://collegeerp442.vercel.app" // frontend URL on Render or Vercel
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://collegeerp442.vercel.app", // frontend URL on Render or Vercel
+    ],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,13 +37,19 @@ app.use("/emailservice", EmailRoute);
 app.use("/staff", staffRoute);
 app.use("/student", studentRoute);
 app.use("/admission", admissionRoute);
-app.use("/staff/admin",adminRoute)
-app.use("/staff/instructor",instructor);
-app.post("/logout",logout)
+app.use("/staff/admin", adminRoute);
+app.use("/staff/instructor", instructor);
+app.post("/logout", logout);
 
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "ERP backend running",
+  });
+});
 // Serve frontend
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Catch-all for SPA routes
 // app.get('/*', (req, res) => {
@@ -50,5 +57,6 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 // });
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
