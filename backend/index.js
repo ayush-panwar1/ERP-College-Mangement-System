baf70752f5c4355a6904a4195d3fea0300a2b64e
silent-hyena <ayushpanwar00007@gmail.com>
2026-03-15 21:33:51 +0530

    configuration change

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,15 @@ import cookieParser from "cookie-parser";
 import serverless from "serverless-http";
 
 // Routes
-import paymentRoute from "../router/makepayment.js";
-import EmailRoute from "../router/sendEmail.js";
-import staffRoute from "../router/staff.js";
-import studentRoute from "../router/student.js";
-import admissionRoute from "../router/admission.js";
-import adminRoute from "../router/admin.js";
-import instructor from "../router/instructor.js";
-import { logout } from "../controller/logoutUser.js";
+import paymentRoute from "./router/makepayment.js";
+import EmailRoute from "./router/sendEmail.js";
+import staffRoute from "./router/staff.js";
+import studentRoute from "./router/student.js";
+import admissionRoute from "./router/admission.js";
+import adminRoute from "./router/admin.js";
+import instructor from "./router/instructor.js";
+import { logout } from "./controller/logoutUser.js";
+
 
 dotenv.config();
 const app = express();
